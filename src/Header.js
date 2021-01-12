7231e032d13766609a8255993b4d764430f75086
Abeer Rao <abeerrao@icloud.com>
2021-01-12 12:00:37 +0530

    Changed Header component and styled the input field, button, icon, and navbar

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
+import {SearchOutlined} from '@material-ui/icons';
 
 function Header() {
     return (
         <nav className="header">
-            <img
-            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            className="header__logo"
-            alt="" />
+            <Link to="/">
+                <img
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                className="header__logo"
+                alt="" />
+            </Link>
+
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchOutlined
+                className="header__searchIcon" />
+            </div>
         </nav>
     )
 }
