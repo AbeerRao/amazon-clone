96673129317055bad52c5231d6d275675f1b8670
Abeer Rao <abeerrao@icloud.com>
2021-01-13 14:58:13 +0530

    Added an example product with styling. Bug: Product not going into image gradient

--- src/Product.js
+++ src/Product.js
@@ -3,8 +3,28 @@ import './Product.css';
 
 function Product({ id, title, price, rating, image }) {
     return (
-        <div>
-
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>₹</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {
+                        Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p>⭐</p>
+                        ))
+                    }
+                </div>
+            </div>
+            <img
+            className="product__image"
+            src={image}
+            alt={title} />
+            <button>Add to basket</button>
         </div>
     )
 }
